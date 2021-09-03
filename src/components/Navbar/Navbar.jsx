0fc8cdec400aef8a1e97d707eddc92fd92e84a21
Christian Marquez <christianmarquez41@gmail.com>
2021-09-03 19:24:53 -0400

    finished navbar elements

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,14 +12,28 @@ import {
 
 export default function Navbar() {
   return (
-  <>
-    <AppBar position="fix" className={classes.appBar} color="inherit">
-      <Toolbar>
-        <Typography>
-            <img src={} alt="Commerce.js" height="25px"  className={classes.image}/>
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  </>
-  )
+    <>
+      <AppBar position="fix" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              src="https://i.ibb.co/Qp1SXBw/commerce.png"
+              alt="Commerce.js"
+              height="25px"
+              className={classes.image}
+            />
+            Commerce.js
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 }
