21c06d0ffc6f4debc4b21dcdb5723c5fd8e9d221
Christian Marquez <christianmarquez41@gmail.com>
2021-09-05 16:24:36 -0400

    finished component that shows a review of the order

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { Typography, Button, Divider } from '@material-ui/core'
-import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
-import Review from "./Review"
+import React from 'react';
+import { Typography, Button, Divider } from '@material-ui/core';
+import { loadStripe } from '@stripe/stripe-js';
+import Review from './Review';
+import {
+  Elements,
+  CardElement,
+  ElementsConsumer,
+} from '@stripe/react-stripe-js';
 
-export default function PaymentForm() {
-    return (
-        <>
-            <Review />
-        </>
-    )
+export default function PaymentForm({ checkoutToken }) {
+  return (
+    <>
+      <Review checkoutToken={checkoutToken} />
+    </>
+  );
 }
